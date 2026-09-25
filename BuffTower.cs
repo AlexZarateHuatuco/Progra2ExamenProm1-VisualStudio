3477c9e1e6fe9132a237b3ecd8c50ac7a903c522
hidekipinesol <hidekiagarie13@gmail.com>
2026-09-24 21:50:43 -0500

    Clase torre pequeña y torre fuerte

---
+++ BuffTower.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progra2ExamenP1_VisualStudio
+{
+    internal class BuffTower : Tower
+    {
+        public BuffTower(string nombre = "Torre Fuerte") : base(nombre, vida: 300, daño: 50)
+        {
+            Console.WriteLine($"{nombreTorre} --- (Vida: {VidaActual}, Daño: {daño}");
+        }
+
+        public override void Atacar(Entities enemigo)
+        {
+            if (VidaActual <= 0)
+            {
+                return;
+            }
+
+            if (enemigo != null)
+            {
+                Console.WriteLine($"{nombreTorre} lanza una bomba que causa {daño} de daño masivo");
+                enemigo.RecibirDaño(daño);
+            }
+        }
+    }
+}
---
+++ SmallTower.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progra2ExamenP1_VisualStudio
+{
+    internal class SmallTower : Tower
+    {
+        public SmallTower(string nombre = "Torre Pequeña") : base(nombre, vida: 100, daño: 15)
+        {
+            Console.WriteLine($"{nombreTorre} --- (Vida: {VidaActual}, Daño: {daño}");
+        }
+
+        public override void Atacar(Entities enemigo)
+        {
+            if (VidaActual <= 0)
+            {
+                return;
+            }
+
+            if (enemigo != null)
+            {
+                Console.WriteLine($"{nombreTorre} dispara una flecha rápida causando {daño} de daño");
+                enemigo.RecibirDaño(daño);
+            }
+        }
+    }
+}
